a8d32c580af99e9678731fcaa88921ffb54f56a0
martinclason123 <martinclason123@yahoo.com>
2020-12-02 15:43:30 -0500

    Updates

---
+++ precache-manifest.517597fff6de5fc1b71bd026c9f6e32c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd390b04b3bff652027b",
+    "url": "/course-directory/static/css/main.99447370.chunk.css"
+  },
+  {
+    "revision": "fd390b04b3bff652027b",
+    "url": "/course-directory/static/js/main.fd390b04.chunk.js"
+  },
+  {
+    "revision": "f1114e3871cf27b0485b",
+    "url": "/course-directory/static/js/1.f1114e38.chunk.js"
+  },
+  {
+    "revision": "527751acb6022af06d5d",
+    "url": "/course-directory/static/js/runtime~main.527751ac.js"
+  },
+  {
+    "revision": "40f03b343c624718cefd3b55e8ee5bc5",
+    "url": "/course-directory/index.html"
+  }
+];
